04d068c99c99b60eccfb0a47b0f4aade2dd3b52d
1mpulseIT <125988539+1mpulseIT@users.noreply.github.com>
2023-02-23 17:46:47 +0500

    Add files via upload

---
+++ Exam/script/footer.js
@@ -0,0 +1,20 @@
+document.write(`
+    <footer class="footer">
+                <div class="footer__div">
+                    <div class="footer__div__content">
+                        <div class="footer__div__content__logo">
+                            <img src="./assets/img/logo (1).png" alt=""><a target="_blank" href="./index.html">Marico</a>
+                            <p>info@marico.co</p>
+                        </div>
+                        <div class="footer__div__content__option">
+                            <div class="footer__div__content__option__links">
+                                <a class="links" target="_blank" href="./about.html">About</a>
+                                <a class="links" target="_blank" href="./pricing.html">Pricing</a>
+                                <a class="links" target="_blank" href="./blog.html">Blog</a>
+                                <a class="links" target="_blank" href="./Signup.html">Sign in</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </footer>
+`);
